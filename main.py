796d163f39d935aefb381e1835e3cdd5b4d67f50
liuhongyao99cs <128149792+liuhongyao99cs@users.noreply.github.com>
2025-09-21 12:20:59 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,71 @@
 import torch
 import torch.nn.functional as F
+import time
 from transformers import AutoModelForCausalLM, AutoTokenizer, PreTrainedModel
+from src.utils import *
+from huggingface_hub import login
 
-model_name = "Qwen/Qwen3-8B"
+model_name = "Qwen/Qwen3-4B"  # 
+model_N = "Qwen3-4B"
+data_name = "Academic"
 
 # ========================
 # Step 0: 加载模型和 tokenizer
 # ========================
 
+login(token = "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")
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     load_in_4bit=True,
-    torch_dtype=torch.float16,
+    dtype=torch.float16, 
+    attn_implementation="flash_attention_2",
     device_map="auto",
-    attn_implementation="eager",
-    output_attentions=True
+    #output_attentions=False
 )
 
-dataset = "C:/Users/14490/Desktop/WiKV/datasets/Academic.jsonl"
-
+dataset = f"/home/hoongyao/data/test_data/{data_name}.jsonl"
 data = load_testcases(dataset)
-print(data[10]['question'])
-
+for session_id in range(data.shape[0]):
+    
+    if data_name == 'longchat':
+        input_text = data[session_id]['prompt']
+    else:
+        input_text = data[session_id]['context']
+    input_text = input_text * 2
+    inputs_ids = tokenizer(input_text, return_tensors="pt").to(model.device)
+    
+    #input_ids = inputs_ids['input_ids'][:,0:20000]
+    #attention_mask = inputs_ids['attention_mask'][:,0:20000]
+    
+    print(input_ids.shape)
+    '''with torch.no_grad():
+        outputs = model(
+            input_ids=input_ids
+        )
+    '''
+    
+    att_extract_(
+        model=model, 
+        model_name=model_N, # "Qwen3-8b"
+        data_name=data_name,  # "longchat"
+        session_id=session_id, # 5-th sample
+        input_ids = input_ids,
+        layer_id=36
+    )
+    
+    '''
+    start_time = time.time()
+    with torch.no_grad():
+        generated = model.generate(
+            input_ids, 
+            max_new_tokens = 1,
+            attention_mask=attention_mask,
+            output_hidden_states=True, 
+            output_scores=True, 
+            return_dict_in_generate=True
+        )
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Prefill time: {elapsed_time:.2f} s for a context with {input_ids.shape[1]} tokens.. \n")
+    '''
